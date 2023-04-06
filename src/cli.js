0fc435e21431a9d094badeb9bdfa8ae88cd11b66
deponiann <g.novikov1034@yandex.ru>
2023-04-06 12:28:07 +0300

    Eslint fix

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-function cli () {
-    const name = readlineSync.question('May I have your name?: ');
-    console.log("Hello, " + (name) + "!");
+function cli() {
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
 }
 
 export default cli;
