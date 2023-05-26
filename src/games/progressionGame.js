8c9b76fe9cb471caca5efc607a2a754a81c927f8
deponiann <g.novikov1034@yandex.ru>
2023-05-26 17:13:28 +0300

    fix

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -5,7 +5,7 @@ export default function progressionGame(username) {
   console.log('What number is missing in the progression?');
   for (let i = 0; i < 3; i += 1) {
     const array = randomArray();
-    const step = Math.floor(Math.random() * (array.length) + 1);
+    const step = Math.floor(Math.random() * (array.length) );
     const correctAnswer = array[step];
     array[step] = '..';
     const string = array.join(' ');
